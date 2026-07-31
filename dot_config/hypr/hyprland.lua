e701c5282ce84673d1261b69835869bb8a3228fd
Maady <maadhesh.off@gmail.com>
2026-07-31 09:25:15 +0530

    switched .conf to .lua for hyprland

---
+++ dot_config/hypr/hyprland.lua
@@ -0,0 +1,12 @@
+---@module 'hl'
+
+
+local io = require("modules/io")
+local generals = require("modules/generals")
+local autostart = require("modules/autostart")
+local env = require("modules/env")
+local windowrules = require("modules/windowrules")
+local plugins = require("modules/plugins")
+local decorations = require("modules/decorations")
+local bindings = require("modules/bindings")
+
